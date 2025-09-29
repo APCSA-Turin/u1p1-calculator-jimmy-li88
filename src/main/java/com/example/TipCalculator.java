fb6f8d942f631471d40d889be26ff099205060c5
Jimmy Li <jli8239@bths.edu>
2025-09-29 16:52:27 +0000

    save

--- src/main/java/com/example/TipCalculator.java
+++ src/main/java/com/example/TipCalculator.java
@@ -6,50 +6,84 @@ public class TipCalculator {
     public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
         //DO NOT DELETE ANY OF THE CODE BELOW      
 
+        double tipAmount = cost * percent / 100;
+        double totalBillWithTip = cost + tipAmount;
+        double costPreTipPerPerson = cost / people;
+        double tipPerPerson = tipAmount / people;
+        double totalCostPerPerson = totalBillWithTip / people;
+       
+
+        // googled how to round two decimals
         String result = "-------------------------------\n" +
-                       "Total bill before tip: $" + "" + "\n" +
-                       "Total percentage: " + "" + "%\n" +
-                       "Total tip: $" + "" + "\n" +
-                       "Total Bill with tip: $" + "" + "\n" +
-                       "Per person cost before tip: $" + "" + "\n" +
-                       "Tip per person: $" + "" + "\n" +
-                       "Total cost per person: $" + "" + "\n" +
+                       "Total bill before tip: $" + Math.round(cost * 100) / 100.0 + "\n" +
+                       "Total percentage: " + percent + "%\n" +
+                       "Total tip: $" + Math.round(tipAmount * 100) / 100.0 + "\n" +
+                       "Total Bill with tip: $" + Math.round(totalBillWithTip * 100) / 100.0 + "\n" +
+                       "Per person cost before tip: $" + Math.round(costPreTipPerPerson * 100) / 100.0 + "\n" +
+                       "Tip per person: $" + Math.round(tipPerPerson * 100) / 100.0 + "\n" +
+                       "Total cost per person: $" + Math.round(totalCostPerPerson * 100) / 100.0 + "\n" +
                        "-------------------------------\n";
 
         return result;
     }
 
     public static String extraCredit(int people, int percent, double cost) {
-        String result = "Extra credit not implemented";
-        boolean condition = true;
-    
-        //  COPY AND PASTE YOUR PROGRAM FROM calculateTip() HERE 
         
+        boolean condition = true;
+        
+        
+        double tipAmount = cost * percent / 100;
+        double totalBillWithTip = cost + tipAmount;
+        double costPreTipPerPerson = cost / people;
+        double tipPerPerson = tipAmount / people;
+        double totalCostPerPerson = totalBillWithTip / people;
+       
+
+        // googled how to round two decimals
+        String result = "-------------------------------\n" +
+                       "Total bill before tip: $" + Math.round(cost * 100) / 100.0 + "\n" +
+                       "Total percentage: " + percent + "%\n" +
+                       "Total tip: $" + Math.round(tipAmount * 100) / 100.0 + "\n" +
+                       "Total Bill with tip: $" + Math.round(totalBillWithTip * 100) / 100.0 + "\n" +
+                       "Per person cost before tip: $" + Math.round(costPreTipPerPerson * 100) / 100.0 + "\n" +
+                       "Tip per person: $" + Math.round(tipPerPerson * 100) / 100.0 + "\n" +
+                       "Total cost per person: $" + Math.round(totalCostPerPerson * 100) / 100.0 + "\n" +
+                       "-------------------------------\n";
         // the while loop condition is checked,
         // and if TRUE, runs the code inside.
         // when the code inside is done running, the condition is rechecked,
         // and the loop repeats as long as the condition remains TRUE.
         // when the condition becomes FALSE, it stops
-
-        while (condition) {
-            //enter your code here 
-            System.out.println("placeholder");
+        Scanner item = new Scanner(System.in);
+        System.out.println("Enter an item name or type '-1' to finish:");
+        String itemName = item.nextLine();
+        String allItems = "";
+        // chatgpted how to store multiple inputs in a variable
+        // googled .equals syntax
+        while (!itemName.equals("-1")) {
+            allItems += itemName + "\n";
+            System.out.println("Enter an item name or type '-1' to finish:");
+            if (item.hasNextLine()) {
+                itemName = item.nextLine();
+            }
+            
             condition = false;
-    
         }
-
-        return result;
+        item.close();
+        System.out.println(result);
+        System.out.println("Items ordered:");
+        return allItems;
+        
     }
-    
     
      //TEST YOUR PROGRAM IN main
      public static void main(String[] args) {
         //try different values for people, percent, and cost to test your program before running test cases
-        int people=10; 
-        int percent=8;
-        double cost=10.5;              
-        //System.out.println(calculateTip(people,percent,cost));
+        int people = 10;
+        int percent = 8;
+        double cost = 10.5;           
+        System.out.println(calculateTip(people,percent,cost));
         System.out.println(extraCredit(people, percent, cost));
     }
 }
-        
+        // 10, 8, 10.5
